e0c9b868f66f2d27a37c146c78b137e944105922
Marc Christophe <fusupo@gmail.com>
2024-05-15 22:21:26 -0700

    add 'Modelling' page @relica/knowledge-integrator-ii

---
+++ packages/knowledge-integrator-ii/src/Modelling.tsx
@@ -0,0 +1,11 @@
+import HSMManager from "@relica/hsm-manager";
+
+const Modelling = () => {
+    return (
+        <div>
+            <h1>Modelling</h1>
+        </div>
+    );
+};
+
+export default Modelling;
