a4fa1f7222c605276231f66ee83397022cfae1cb
Marc Christophe <fusupo@gmail.com>
2024-06-01 11:56:52 -0700

    woriking on physical object form

--- packages/knowledge-integrator-ii/src/pages/Modelling/definition/PhysicalObject.tsx
+++ packages/knowledge-integrator-ii/src/pages/Modelling/definition/PhysicalObject.tsx
@@ -401,21 +401,21 @@ const Modelling = (props: any) => {
         console.log("SUCCESS", success);
         //submit the facts
 
-        // const foo = facts.map((fact) => {
-        //   return {
-        //     ...fact,
-        //     lh_object_uid: parseInt(fact.lh_object_uid),
-        //     fact_uid: parseInt(fact.fact_uid),
-        //     rel_type_uid: parseInt(fact.rel_type_uid),
-        //     rh_object_uid: parseInt(fact.rh_object_uid),
-        //     uom_uid: parseInt(fact.uom_uid),
-        //   };
-        // });
-        // const result = axios.post(
-        //   "http://localhost:3000" + SUMBIT_BINARY_FACTS_ENDPOINT,
-        //   foo
-        // );
-        // console.log("RESULT", result);
+        const foo = facts.map((fact) => {
+          return {
+            ...fact,
+            lh_object_uid: parseInt(fact.lh_object_uid),
+            fact_uid: parseInt(fact.fact_uid),
+            rel_type_uid: parseInt(fact.rel_type_uid),
+            rh_object_uid: parseInt(fact.rh_object_uid),
+            uom_uid: parseInt(fact.uom_uid),
+          };
+        });
+        const result = axios.post(
+          "http://localhost:3000" + SUMBIT_BINARY_FACTS_ENDPOINT,
+          foo
+        );
+        console.log("RESULT", result);
       }
 
       console.log("SUCCESS", success);
