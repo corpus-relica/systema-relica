14916c38b79beb67921a6e5318e7efa826341c1f
Marc Christophe <fusupo@gmail.com>
2024-06-11 10:31:26 -0700

    attempt relax permissions on processed csv

--- packages/archivist/src/file/file.service.ts
+++ packages/archivist/src/file/file.service.ts
@@ -3,33 +3,33 @@ import { promises as fsPromises } from 'node:fs';
 
 @Injectable()
 export class FileService {
-    private readonly logger = new Logger(FileService.name);
+  private readonly logger = new Logger(FileService.name);
 
-    constructor() {}
+  constructor() {}
 
-    async writeFileAsync(filename: string, data: any) {
-        this.logger.verbose('writeFileAsync');
-        try {
-            await fsPromises.writeFile(filename, data);
-            this.logger.verbose('File written successfully');
-        } catch (error) {
-            this.logger.error('Error writing file:', error);
-        }
+  async writeFileAsync(filename: string, data: any) {
+    this.logger.verbose('writeFileAsync');
+    try {
+      await fsPromises.writeFile(filename, data, { mode: 0o755 });
+      this.logger.verbose('File written successfully');
+    } catch (error) {
+      this.logger.error('Error writing file:', error);
     }
+  }
 
-    async processFilesInDirectory(
-        directory: string,
-        func: (filename: string) => Promise<void>,
-    ) {
-        try {
-            const files = await fsPromises.readdir(directory);
-            const csvFiles = files.filter((file) => file.endsWith('.csv'));
+  async processFilesInDirectory(
+    directory: string,
+    func: (filename: string) => Promise<void>,
+  ) {
+    try {
+      const files = await fsPromises.readdir(directory);
+      const csvFiles = files.filter((file) => file.endsWith('.csv'));
 
-            for (const file of csvFiles) {
-                await func(file);
-            }
-        } catch (err) {
-            this.logger.error('Error processing files:', err);
-        }
+      for (const file of csvFiles) {
+        await func(file);
+      }
+    } catch (err) {
+      this.logger.error('Error processing files:', err);
     }
+  }
 }
