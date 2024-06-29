a1ff44af2783782e34bde6d5d1dc46ef47e652b7
Marc Christophe <fusupo@gmail.com>
2024-06-29 00:09:06 -0700

    kindContextMenu 75% parity with integrator(i)

--- packages/knowledge-integrator-ii/src/components/GraphContextMenu/KindContextMenu.tsx
+++ packages/knowledge-integrator-ii/src/components/GraphContextMenu/KindContextMenu.tsx
@@ -11,6 +11,14 @@ import Divider from "@mui/material/Divider";
 
 import { sockSendCC } from "../../socket";
 
+import {
+  getAllRelatedFacts,
+  getSubtypes,
+  getSubtypesCone,
+  getClassified,
+  getSpecializationHierarchy,
+} from "../../RLCBaseClient";
+
 const SH = "SH";
 const SHOW_CLASSIFIED = "show classified";
 const SHOW_ALL = "show 'all'";
@@ -33,11 +41,14 @@ interface KindContextMenuProps {
 // const handleItemClick = (e) => {
 // };
 
+import { useStores } from "../../context/RootStoreContext";
+
 const KindContextMenu: React.FC<KindContextMenuProps> = (props) => {
+  const { factDataStore } = useStores();
   const { uid, open, handleClose, x, y } = props;
 
   const handleItemClick = useCallback(
-    (e) => {
+    async (e) => {
       const value = e.currentTarget.getAttribute("value");
       console.log(`Clicked item with value: ${value}`);
       switch (value) {
@@ -46,28 +57,65 @@ const KindContextMenu: React.FC<KindContextMenuProps> = (props) => {
           handleClose();
           break;
         case SHOW_CLASSIFIED:
-          // queryClassified();
+          const classified = await getClassified(uid);
+          const existingClassified = classified
+            .filter((individual: any) => {
+              const foo = factDataStore.findDefinitiveFacts(
+                individual.lh_object_uid
+              );
+              return (
+                factDataStore.findDefinitiveFacts(individual.lh_object_uid)
+                  .length > 0
+              );
+            })
+            .map((individual) => individual.lh_object_uid);
+          // setPossibleClassified(classified);
+          // setExistingClassified(existingClassified);
+          // setClassifiedModalIsOpen(true);
+          // const response = await getClassified(uid);
+          // addFacts(response);
+          console.log("MUTHERFUCK??");
+          handleClose();
           break;
         case SHOW_ALL:
           sockSendCC("user", "getAllRelatedFacts", { uid });
           handleClose();
-          // queryOmni();
           break;
         case SHOW_SUBTYPES:
-          // querySubtypes();
+          // sockSendCC("user", "getSubtypes", { uid });
+          const subtypes = await getSubtypes(uid);
+          const existingSubtypes = subtypes
+            .filter((subtype) => {
+              const foo = factDataStore.findDefinitiveFacts(
+                subtype.lh_object_uid
+              );
+              return (
+                factDataStore.findDefinitiveFacts(subtype.lh_object_uid)
+                  .length > 0
+              );
+            })
+            .map((subtype) => subtype.lh_object_uid);
+          // setPossibleSubtypes(subtypes);
+          // setExistingSubtypes(existingSubtypes);
+          // setSubtypesModalIsOpen(true);
+          handleClose();
           break;
         case SHOW_SUBTYPES_CONE:
-          // querySubtypesCone();
+          sockSendCC("user", "getSubtypesCone", { uid });
+          handleClose();
           break;
         case REM_THIS:
-          // removeEntity();
+          sockSendCC("user", "removeEntity", { uid });
+          handleClose();
           break;
         case REM_SUBTYPES_R:
-          // removeEntitySubtypesRecursive();
+          sockSendCC("user", "removeEntitySubtypesRecursive", { uid });
+          handleClose();
           break;
         case DELETE_THIS:
           // setUidToDelete(uid);
           // setWarnIsOpen(true);
+          handleClose();
           break;
 
         default:
@@ -109,13 +157,21 @@ const KindContextMenu: React.FC<KindContextMenuProps> = (props) => {
       <MenuItem value={SHOW_ALL} onClick={handleItemClick}>
         show 'all'
       </MenuItem>
-      <MenuItem value={SHOW_SUBTYPES}>show subtypes</MenuItem>
-      <MenuItem value={SHOW_SUBTYPES_CONE}>show subtypes cone</MenuItem>
+      <MenuItem value={SHOW_SUBTYPES} onClick={handleItemClick}>
+        show subtypes
+      </MenuItem>
+      <MenuItem value={SHOW_SUBTYPES_CONE} onClick={handleItemClick}>
+        show subtypes cone
+      </MenuItem>
       <Divider />
-      <MenuItem value={REM_THIS}>rem this</MenuItem>
-      <MenuItem value={REM_SUBTYPES_R}>rem subtypes(r)</MenuItem>
+      <MenuItem value={REM_THIS} onClick={handleItemClick}>
+        rem this
+      </MenuItem>
+      <MenuItem value={REM_SUBTYPES_R} onClick={handleItemClick}>
+        rem subtypes(r)
+      </MenuItem>
       <Divider />
-      <MenuItem value={DELETE_THIS} className={menuItemClassName}>
+      <MenuItem value={DELETE_THIS} className={menuItemClassName} disabled>
         delete this!
       </MenuItem>
     </Menu>
