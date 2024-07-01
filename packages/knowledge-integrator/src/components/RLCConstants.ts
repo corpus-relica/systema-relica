bd82b05ff0d0a7957bfb80945fb996c4936828d1
Marc Christophe <fusupo@gmail.com>
2024-06-30 17:22:36 -0700

    remove knowledge-integrator(i)

--- packages/knowledge-integrator/src/components/RLCConstants.ts
+++
@@ -1,3 +0,0 @@
-export const RLC_MEMO_TYPE_UID = 303;
-export const RLC_MEMO_TITLE_TYPE_UID = 305;
-export const RLC_MEMO_BODY_TYPE_UID = 304;
--- packages/knowledge-integrator/vite.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import VitePluginChecker from "vite-plugin-checker";
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react(), VitePluginChecker({ typescript: true })],
-  envDir: "./",
-});
