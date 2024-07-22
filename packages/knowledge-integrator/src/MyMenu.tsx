a1da98a1177d80cf6c43c17fbd98e890f3a55a4c
Marc Christophe <fusupo@gmail.com>
2024-07-22 16:18:56 -0700

    update menu icons

--- packages/knowledge-integrator/src/MyMenu.tsx
+++ packages/knowledge-integrator/src/MyMenu.tsx
@@ -1,15 +1,23 @@
 import { Menu } from "react-admin";
 import LabelIcon from "@mui/icons-material/Label";
+import TravelExploreIcon from "@mui/icons-material/TravelExplore";
+import BlurCircularIcon from "@mui/icons-material/BlurCircular";
+import PlumbingIcon from "@mui/icons-material/Plumbing";
+import FoundationIcon from "@mui/icons-material/Foundation";
 
 export const MyMenu = () => (
   <Menu>
     <Menu.DashboardItem />
     <Menu.ResourceItem name="db/kinds" />
-    <Menu.Item to="env/graph" primaryText="Graph" leftIcon={<LabelIcon />} />
+    <Menu.Item
+      to="env/graph"
+      primaryText="Graph"
+      leftIcon={<BlurCircularIcon />}
+    />
     <Menu.Item
       to="/modelling"
       primaryText="Modelling"
-      leftIcon={<LabelIcon />}
+      leftIcon={<PlumbingIcon />}
     />
   </Menu>
 );
