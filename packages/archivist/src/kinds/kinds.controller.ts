06dcfd793570020684f4c48147a5515742affa5a
Marc Christophe <fusupo@gmail.com>
2024-09-21 10:41:14 -0700

    return correct totals on getKinds endpoint

--- packages/archivist/src/kinds/kinds.controller.ts
+++ packages/archivist/src/kinds/kinds.controller.ts
@@ -1,42 +1,45 @@
 import {
-    Controller,
-    Get,
-    Query,
-    Inject,
-    Param,
-    Logger,
-    Res,
+  Controller,
+  Get,
+  Query,
+  Inject,
+  Param,
+  Logger,
+  Res,
 } from '@nestjs/common';
 import { Response } from 'express';
 import { KindsService } from './kinds.service';
 
 @Controller('kinds')
 export class KindsController {
-    private readonly logger = new Logger(KindsController.name);
+  private readonly logger = new Logger(KindsController.name);
 
-    constructor(private readonly kindsService: KindsService) {}
+  constructor(private readonly kindsService: KindsService) {}
 
-    @Get()
-    async getKinds(
-        @Query('sort') sort: string,
-        @Query('range') range: string,
-        @Query('filter') filter: string,
-        @Res({ passthrough: true }) res: Response,
-    ) {
-        const [sortField, sortOrder] = JSON.parse(sort);
-        const [minRange, maxRange] = JSON.parse(range);
-        const skip = minRange;
-        const take = maxRange - minRange;
+  @Get()
+  async getKinds(
+    @Query('sort') sort: string,
+    @Query('range') range: string,
+    @Query('filter') filter: string,
+    @Res({ passthrough: true }) res: Response,
+  ) {
+    const [sortField, sortOrder] = JSON.parse(sort);
+    const [minRange, maxRange] = JSON.parse(range);
+    const skip = minRange;
+    const take = maxRange - minRange;
 
-        const result = await this.kindsService.getList(
-            sortField,
-            sortOrder,
-            skip,
-            take,
-        );
+    const result = await this.kindsService.getList(
+      sortField,
+      sortOrder,
+      skip,
+      take,
+    );
 
-        res.header('Content-Range', `posts ${minRange}-${maxRange}/319`);
+    res.header(
+      'Content-Range',
+      `posts ${minRange}-${maxRange}/${result.total}`,
+    );
 
-        return result;
-    }
+    return result;
+  }
 }
