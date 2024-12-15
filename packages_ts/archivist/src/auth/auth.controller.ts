f3f2480dd9ed56bfae0c5c36cdd7089aa4baceff
Marc Christophe <fusupo@gmail.com>
2024-12-15 12:01:10 -0800

    add 'auth/validate' endpoint

--- packages_ts/archivist/src/auth/auth.controller.ts
+++ packages_ts/archivist/src/auth/auth.controller.ts
@@ -1,11 +1,13 @@
 import {
   Body,
+  Headers,
   Controller,
   Get,
   HttpCode,
   HttpStatus,
   Post,
   Request,
+  Logger,
   UseGuards,
 } from '@nestjs/common';
 import { AuthGuard } from './auth.guard';
@@ -14,6 +16,7 @@ import { Public } from './auth.decorator';
 
 @Controller('auth')
 export class AuthController {
+  private readonly logger = new Logger(AuthController.name);
   constructor(private authService: AuthService) {}
 
   @Public()
@@ -21,6 +24,22 @@ export class AuthController {
   @Post('login')
   signIn(@Body() signInDto: Record<string, any>) {
     return this.authService.signIn(signInDto.username, signInDto.password);
+  }
+
+  @Public()
+  @Post('validate')
+  async validate(@Headers() validateDto: Record<string, any>) {
+    const { authorization } = validateDto;
+    const token = authorization.split(' ')[1];
+
+    return true;
+    // try {
+    //   return await this.authService.validate(token);
+    // } catch (e) {
+    //   this.logger.error('Error validating token /////////////////////////');
+    //   this.logger.error(e);
+    //   return e;
+    // }
   }
 
   @UseGuards(AuthGuard)
--- packages_ts/archivist/src/auth/auth.service.ts
+++ packages_ts/archivist/src/auth/auth.service.ts
@@ -1,9 +1,11 @@
-import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { Injectable, UnauthorizedException, Logger } from '@nestjs/common';
 import { UsersService } from '../users/users.service';
 import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
+  private readonly logger = new Logger(AuthService.name);
+
   constructor(
     private usersService: UsersService,
     private jwtService: JwtService,
@@ -21,5 +23,18 @@ export class AuthService {
     return {
       access_token: await this.jwtService.signAsync(payload),
     };
+  }
+
+  async validate(token: string): Promise<{ username: string }> {
+    // try {
+    const payload = await this.jwtService.verifyAsync(token);
+    this.logger.log('validate', payload);
+    return { username: payload.username };
+    // } catch (e) {
+    //   this.logger.error('Error validating token /////////////////////////');
+    //   this.logger.error(e);
+    //   // throw new UnauthorizedException();
+    //   throw e;
+    // }
   }
 }
