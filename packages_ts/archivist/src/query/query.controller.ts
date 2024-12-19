d54275a35894e0a855157a4801dfba6921ae0b05
Marc Christophe <fusupo@gmail.com>
2024-12-18 16:44:25 -0800

    Refactor server management and update route specifications

--- packages_ts/archivist/src/query/query.controller.ts
+++ packages_ts/archivist/src/query/query.controller.ts
@@ -7,13 +7,10 @@ import { GellishBaseService } from 'src/gellish-base/gellish-base.service';
 import { Fact } from '@relica/types';
 import { QueryStringDto } from './queryString.dto';
 
-import { GellishParser } from './GellishParser2';
-
 @ApiTags('Query')
 @Controller('query')
 export class QueryController {
   private readonly logger = new Logger(QueryController.name);
-  private readonly parser = new GellishParser();
 
   constructor(
     private readonly queryService: QueryService,
@@ -25,7 +22,7 @@ export class QueryController {
     @Body() queryTable: Fact[], //GellishTable,
   ): Promise<any> {
     // Process the query and return results
-    return this.queryService.interpretTable(queryTable, 1, 10);
+    return this.queryService.interpretQueryTable(queryTable, 1, 10);
   }
 
   @Post('queryString')
@@ -33,32 +30,6 @@ export class QueryController {
   @ApiBody({ type: QueryStringDto })
   async handleGellishQueryString(@Body() body: QueryStringDto): Promise<any> {
     const { queryString, page, pageSize } = body;
-    // Process the query and return results
-    const queryStringArray: string[] = queryString.split('\n');
-
-    let qStr = '';
-    let finalArray = [];
-    queryStringArray.forEach((queryString) => {
-      qStr += queryString + '\n';
-      if (!queryString.startsWith('@')) {
-        finalArray.push(qStr);
-        qStr = '';
-      }
-    });
-
-    const queryTable = finalArray.reduce(
-      (memo, queryString) => memo.concat(this.parser.parse(queryString)),
-      [],
-    );
-
-    this.logger.log('Query Table:', queryTable);
-
-    const result = await this.queryService.interpretTable(
-      queryTable,
-      page,
-      pageSize,
-    );
-    return result;
-    // return queryTable;
+    return this.queryService.interpretQueryString(queryString, page, pageSize);
   }
 }
