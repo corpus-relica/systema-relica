c63b06f24e85110a1787dea69467e46a19cf08d3
K Mills <mustkre8@gmail.com>
2025-04-07 17:07:44 -0700

    Lock UI to dark mode by forcing dark class on document element (#37)

--- packages_ts/frontend/viewfinder/src/index.tsx
+++ packages_ts/frontend/viewfinder/src/index.tsx
@@ -6,6 +6,9 @@ import "./style.css";
 import RootStoreContext from "./context/RootStoreContext";
 import rootStore from "./stores/RootStore";
 
+// Set dark mode on app initialization
+document.documentElement.classList.add('dark');
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <RootStoreContext.Provider value={rootStore}>
     <React.StrictMode>
