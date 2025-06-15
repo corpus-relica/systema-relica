ad2e4fbd2506731f1a3f1d2b3b759b77843e65b7
marc christophe <fusupo@gmail.com>
2025-06-15 08:14:07 -0700

    chore: remove portal dist files from version control

--- packages/portal/dist/auth/index.d.ts
+++
@@ -1,4 +0,0 @@
-export { AuthMiddleware, AuthenticatedRequest } from '../middleware/auth.middleware';
-export { AuthGuard } from '../guards/auth.guard';
-export { User } from '../decorators/user.decorator';
-export { Public } from '../decorators/public.decorator';
--- packages/portal/dist/guards/auth.guard.d.ts
+++
@@ -1,9 +0,0 @@
-import { CanActivate, ExecutionContext } from '@nestjs/common';
-import { Reflector } from '@nestjs/core';
-import { ShutterWebSocketClientService } from '../services/shutter-websocket-client.service';
-export declare class AuthGuard implements CanActivate {
-    private readonly shutterClient;
-    private readonly reflector;
-    constructor(shutterClient: ShutterWebSocketClientService, reflector: Reflector);
-    canActivate(context: ExecutionContext): Promise<boolean>;
-}
