67656d702d48cb95d4cae04e440669b2254a5f62
marc christophe <fusupo@gmail.com>
2025-07-01 09:25:29 -0700

    ♻️ refactor: split IndividualDetails into category-specific components

---
+++ packages/knowledge-integrator/src/components/SelectionDetails/IndividualDetails/Relation.tsx
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from "react";
+
+const IndividualRelationDetails: React.FC = (data) => {
+  const {category} = data
+
+  console.log("IndividualRelationDetails", data)
+
+  return <div>Relation</div>;
+};
+
+export default IndividualRelationDetails;
